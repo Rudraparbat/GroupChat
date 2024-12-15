05e4492d69320414bceb016a400ed789431c2703
Ruddy <rudraparbat714@gmail.com>
2024-12-15 19:29:10 +0530

    set origins

--- chats/settings.py
+++ chats/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,13 +80,13 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',  
     'https://groupchat-igdq.onrender.com',
 ]
-
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/1")
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': "redis", 
+        'LOCATION': REDIS_URL, 
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'KEY_PREFIX': 'cache_'
@@ -99,7 +99,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("redis",6379)],  
+            "hosts": [REDIS_URL],  
             'prefix': 'channels_' 
         },
     },
