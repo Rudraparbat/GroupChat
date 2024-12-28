60897ddca70e45b8c88309adc4b56f6c4267c645
Ruddy <rudraparbat714@gmail.com>
2024-12-28 19:49:46 +0530

    set middleware

--- conn/middlewares.py
+++ conn/middlewares.py
@@ -6,8 +6,12 @@ class JWTmiddleware(BaseMiddleware) :
     async def  __call__(self , scope , recieve , send) :
         cookies = scope['headers'][10]
         decode_cookie = cookies[1].decode('utf-8')
-        cookies = dict(item.split('=') for item in decode_cookie.split('; '))
-        access_token = cookies.get('access_token')
+        print(decode_cookie)
+        try :
+            cookies = dict(item.split('=') for item in decode_cookie.split('; '))
+            access_token = cookies.get('access_token')
+        except :
+            access_token  = None
         if access_token :
             token_data = self.decoder(access_token)
             scope['user'] ={
